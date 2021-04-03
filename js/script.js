b66b402056d96fcbafbc51fbfa27867e92a9bff9
Gusevich <grachovas78@gmail.com>
2021-04-03 18:30:41 +0300

    finished project

--- js/script.js
+++ js/script.js
@@ -2,14 +2,39 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-        }else{
+        } else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({scrollTop: 0});
+    });
+
     // menu/navbar script
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
+    });
+
+    // typing animation script
+    let typed = new Typed('.typing', {
+       strings: ['FrontEnd Developer', 'Philosopher', 'AnimeBoy', 'Sportsman'],
+       typeSpeed: 100,
+       backSpeed: 60,
+       loop: true
+    });
+    let typed2 = new Typed('.typing-2', {
+        strings: ['FrontEnd Developer', 'Philosopher', 'AnimeBoy', 'Sportsman'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // owl carousel script
